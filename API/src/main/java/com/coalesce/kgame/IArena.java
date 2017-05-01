2b27e2d94c076efb57539ea65bacbd7f8ea0cbe3
Mariell Hoversholm <proximyst@proximyst.com>
2017-05-01 18:56:37 +0200

    Add more default methods and documentation

--- API/src/main/java/com/coalesce/kgame/IArena.java
+++ API/src/main/java/com/coalesce/kgame/IArena.java
@@ -1,12 +1,16 @@
 package com.coalesce.kgame;
 
+import com.coalesce.kgame.utilities.BukkitUtils;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * An arena which is being used by a minigame.
@@ -47,6 +51,27 @@ public interface IArena extends ICommons, Listener {
      * @since 1.0
      */
     Location getMax();
+
+    /**
+     * Gets all chunks min through max is in.
+     *
+     * @return All the chunks the arena is in.
+     * @since 1.0
+     */
+    default Set<Chunk> getChunks() {
+        Set<Chunk> chunks = new HashSet<>();
+        Location at = getMin().clone();
+        while (at.getX() < getMax().getX()) {
+            chunks.add(at.getChunk());
+            at = at.add(16, 0, 0);
+        }
+        at = getMin().clone();
+        while (at.getZ() < getMax().getZ()) {
+            chunks.add(at.getChunk());
+            at = at.add(0, 0, 16);
+        }
+        return chunks;
+    }
 
     /**
      * Gets the maximum players the arena can have before it's full.
@@ -100,5 +125,12 @@ public interface IArena extends ICommons, Listener {
      * @return The arena participators.
      * @since 1.0
      */
-    Set<Player> participators();
+    default Set<Player> participators() {
+        Set<Player> players = new HashSet<>();
+        if (getMin().getWorld() != getMax().getWorld()) {
+            throw new IllegalStateException("Min world cannot be in a different world from max.");
+        }
+        getChunks().stream().map(Chunk::getEntities).forEach(array -> Arrays.stream(array).filter(e -> e instanceof Player).map(e -> (Player) e).forEach(players::add));
+        return players.stream().filter(player -> BukkitUtils.within(player.getLocation(), getMin(), getMax())).collect(Collectors.toSet());
+    }
 }
